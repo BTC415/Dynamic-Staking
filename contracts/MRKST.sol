33520ea8fd2c13b62a0879965a78ee3f74d2a37c
LovelyBTC <alenschmitt415@gmail.com>
2024-12-14 08:11:19 -0600

    [feat] wrote deploy scripts

--- contracts/MRKST.sol
+++ contracts/MRKST.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.28;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract MRKToken is ERC20 {
+contract MRKST is ERC20 {
     constructor() ERC20("MARK-STAKING", "MRKST") {
         _mint(msg.sender, 100000000 * 10 ** 18);
     }
